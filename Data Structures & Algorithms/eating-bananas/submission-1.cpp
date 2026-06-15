49cc954e954e53e8048c0ceab33f811414813d11
Vedant Gupta <49195734+veds-g@users.noreply.github.com>
2026-06-15 19:46:36 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& p, int h) {
+        int mx = *max_element(p.begin(), p.end());
+        if (p.size() == h) return mx;
+
+        int l = 1, r = mx, res = INT_MAX;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            long long totalTime = 0;
+            for (int x : p) {
+                totalTime += ceil(static_cast<double>(x) / mid);
+            }
+            if (totalTime > h) l = mid + 1;
+            else res = mid, r = mid - 1;
+        }
+        return res;
+    }
+};
