0752887c72d8e5e8248dfa91c406b481807224c6
Vedant Gupta <49195734+veds-g@users.noreply.github.com>
2026-06-30 10:55:38 +0530

    Add: delete-node-in-a-bst - submission-3

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-3.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        TreeNode* p = NULL, * curr = root;
+
+        while (curr and curr -> val != key) {
+            p = curr;
+            if (curr -> val < key) curr = curr -> right;
+            else curr = curr -> left;
+        }
+
+        if (!curr) return root;
+
+        if (!curr -> left || !curr -> right) {
+            TreeNode* child = curr -> left ? curr -> left : curr -> right;
+            if (!p) {
+                return child;
+            }
+            if (p -> left == curr) {
+                p -> left = child;
+            } else {
+                p -> right = child;
+            }
+            return root;
+        }
+
+        TreeNode* r = curr -> right, * rp = NULL;
+
+        while (r and r -> left) {
+            rp = r;
+            r = r -> left;
+        }
+
+        if (rp) {
+            rp -> left = r -> right;
+            r -> right = curr -> right;
+        }
+
+        r -> left = curr -> left;
+
+        if (!p) {
+            return r;
+        }
+        
+        if (p -> left == curr) p -> left = r;
+        else p -> right = r;
+
+        return root;
+    }
+};
