b3f070477dfdb6230cb8fd3ebcb3c9ef788b0754
Vedant Gupta <49195734+veds-g@users.noreply.github.com>
2026-07-07 12:17:34 +0530

    Add: search-for-word-ii - submission-2

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-2.cpp
@@ -0,0 +1,72 @@
+class TrieNode {
+public:
+    unordered_map<char, TrieNode*> children;
+    bool terminal = false;
+    string word = "";
+};
+
+class Trie {
+public:
+    TrieNode* root;
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void addWord(string s) {
+        TrieNode* curr = root;
+        for(auto& x : s) {
+            if (!curr -> children.contains(x)) {
+                curr -> children[x] = new TrieNode();
+            }
+            curr = curr -> children[x];
+        }
+        curr -> terminal = true;
+        curr -> word = s;
+    }
+};
+
+class Solution {
+public:
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        Trie trie;
+        for(auto& x : words) {
+            trie.addWord(x);
+        }
+
+        int m = board.size(), n = board[0].size();
+
+        set<string> res;
+        for(int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dfs(board, i, j, trie.root, res);
+            }
+        }
+
+        return vector<string>(res.begin(), res.end());
+    }
+
+private:
+    void dfs(vector<vector<char>>& board, int r, int c, TrieNode* node, set<string>& res) {
+        if (r < 0 || c < 0 || r >= board.size() || c >= board[0].size() || board[r][c] == '#') return;
+        
+        char temp = board[r][c];
+        if (!node -> children.contains(temp)) return;
+
+        TrieNode* next = node -> children[temp];
+        if (next -> terminal) {
+            res.insert(next -> word);
+        }
+
+        board[r][c] = '#';
+
+        int dx[] = {1, 0, -1, 0};
+        int dy[] = {0, 1, 0, -1};
+
+        for (int i = 0; i < 4; i++) {
+            dfs(board, r + dx[i], c + dy[i], next, res);
+        }
+
+        board[r][c] = temp;
+        return;
+    }
+};
