fbc88a7296ab670ea4cb9adc14aaecf982741035
Vedant Gupta <49195734+veds-g@users.noreply.github.com>
2026-07-08 14:38:47 +0530

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        int ans = 1;
+
+        unordered_map<char, int> mp;
+        for (auto& x : tasks) mp[x]++;
+
+        priority_queue<pair<int, char>> pq;
+        for (auto& x : mp) pq.push({x.second, x.first});
+
+        unordered_map<char, int> temp;
+        unordered_set<int> present;
+        int idx = 1;
+        while (!pq.empty()) {
+            pair<int, char> p = pq.top();
+            pq.pop();
+
+            if (temp.contains(p.second)) {
+                int charIdx = temp[p.second];
+                int newIdx = charIdx + n + 1;
+                while (present.contains(newIdx)) newIdx++;
+                temp[p.second] = newIdx;
+                present.insert(newIdx);
+                ans = max(ans, newIdx);
+            } else {
+                while(present.contains(idx)) idx++;
+                temp[p.second] = idx;
+                present.insert(idx);
+                ans = max(ans, idx);
+            }
+
+            if (p.first - 1 != 0) pq.push({p.first - 1, p.second});
+        }
+
+        return ans;
+    }
+};
