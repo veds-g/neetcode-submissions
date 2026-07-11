1785ffc5a37fe3e7d81d6a120a864bb060a3c6b2
Vedant Gupta <49195734+veds-g@users.noreply.github.com>
2026-07-12 02:30:57 +0530

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(1, n, k, temp, ans);
+        return ans;
+    }
+
+    void solve(int i, int n, int k, vector<int>& temp, vector<vector<int>>& ans) {
+        if (temp.size() == k) {
+            ans.push_back(temp);
+            return;
+        }
+
+        if (i > n) {
+            return;
+        }
+
+        solve(i + 1, n, k, temp, ans);
+
+        temp.push_back(i);
+        solve(i + 1, n, k, temp, ans);
+        temp.pop_back();
+    }
+};
