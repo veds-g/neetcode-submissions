f3fba90111edf55315c61ccede3a26f2f72c3eb3
Vedant Gupta <49195734+veds-g@users.noreply.github.com>
2026-07-16 21:36:04 +0530

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        solve(nums, 0, ans);
+        return ans;
+    }
+
+    void solve(vector<int>& nums, int idx, vector<vector<int>>& ans) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[i], nums[idx]);
+            solve(nums, idx + 1, ans);
+            swap(nums[i], nums[idx]);
+        }
+
+    }
+};
