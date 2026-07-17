f3400dd0e1b6a60cc655feb675a1add056b9250f
Vedant Gupta <49195734+veds-g@users.noreply.github.com>
2026-07-17 20:25:09 +0530

    Add: word-break-ii - submission-1

---
+++ Data Structures & Algorithms/word-break-ii/submission-1.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+
+        vector<string> ans;
+        string curr = "", temp = "";
+        solve(0, s, curr, temp, ans, wordSet);
+        return ans;
+    }
+
+    void solve(int idx, string& s, string& curr, string& temp, vector<string>& ans, unordered_set<string>& wordSet) {
+        if (idx == s.size()) {
+            if (curr.empty()) {
+                ans.push_back(temp);
+            }
+            return;
+        }
+
+        curr.push_back(s[idx]);
+
+        if (wordSet.contains(curr)) {
+            string saved = curr;
+            string savedTemp = temp;
+
+            if (!temp.empty()) temp.push_back(' ');
+            temp += curr;
+            curr = "";
+
+            solve(idx + 1, s, curr, temp, ans, wordSet);
+
+            curr = saved;
+            temp = savedTemp;
+        }
+
+        solve(idx + 1, s, curr, temp, ans, wordSet);
+
+        curr.pop_back();
+    }
+};
