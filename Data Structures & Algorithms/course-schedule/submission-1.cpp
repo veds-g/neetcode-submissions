eff9db1644c33e9b670403fce58c8a6bd917c1b1
Vedant Gupta <49195734+veds-g@users.noreply.github.com>
2026-07-24 10:53:37 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> vis(numCourses, 0);
+
+        unordered_map<int, vector<int>> graph;
+
+        for(auto& x : prerequisites) {
+            graph[x[1]].push_back(x[0]);
+        }
+
+        for(int i = 0; i < numCourses; i++) {
+            if (!dfs(i, vis, graph)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool dfs(int idx, vector<int>& vis, unordered_map<int, vector<int>>& graph) {
+        if (vis[idx] == 1) {
+            return false;
+        }
+
+        vis[idx] = 1;
+        for(auto& x : graph[idx]) {
+            if(!dfs(x, vis, graph)) {
+                return false;
+            }
+        }
+
+        vis[idx] = 0;
+        return true;
+    }
+};
