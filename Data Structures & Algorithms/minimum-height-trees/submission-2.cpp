c06157f5cb313c90da3551aa933e36143683a983
Vedant Gupta <49195734+veds-g@users.noreply.github.com>
2026-07-27 23:43:58 +0530

    Add: minimum-height-trees - submission-2

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-2.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+    unordered_map<int, vector<int>> adj;
+    vector<int> vis;
+
+    int dfs(int node) {
+        vis[node] = 1;
+
+        int h = INT_MIN;
+        for(auto x : adj[node]) {
+            if(vis[x] == 1) continue;
+            int currH = dfs(x);
+            if(currH > h) {
+                h = currH;
+            }
+        }
+
+        vis[node] = 0;
+        return h == INT_MIN ? 0 : h + 1;
+    }
+
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        vis.resize(n, 0);
+        for(auto& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+
+        int h = INT_MAX;
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {
+            int height = dfs(i);
+
+            if(height < h) {
+                h = height;
+                ans.clear();
+                ans.push_back(i);
+            } else if (height == h) {
+                ans.push_back(i);
+            }
+        }
+
+        return ans;
+    }
+};
