89f85dc95e85ce2edc656c59ff46a9687ee4e786
Vedant Gupta <49195734+veds-g@users.noreply.github.com>
2026-08-09 09:29:17 +0530

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> ans;
+        int n = s.size();
+
+        unordered_map<char, int> mp;
+        for (int i = 0; i < n; i++) {
+            mp[s[i]] = i;
+        }
+
+        int maxIdx = 0, prevIdx = 0;
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            maxIdx = max(maxIdx, mp[ch]);
+
+            if (maxIdx == i) {
+                ans.push_back(maxIdx - prevIdx + 1);
+                prevIdx = i + 1;
+            }
+        }
+
+        return ans;
+    }
+};
