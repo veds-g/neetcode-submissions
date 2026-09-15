01d7d24b1e313916e0859cf1276cb8fa9e710d13
Vedant Gupta <49195734+veds-g@users.noreply.github.com>
2026-09-15 12:13:20 +0530

    Add: sqrtx - submission-4

---
+++ Data Structures & Algorithms/sqrtx/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l = 0, r = x;
+        while (l <= r) {
+            long long m = l + (r - l) / 2;
+
+            if (m * m <= x) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return r;
+    }
+};
